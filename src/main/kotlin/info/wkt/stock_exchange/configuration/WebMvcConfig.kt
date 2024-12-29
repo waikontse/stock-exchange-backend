55741ee03f1f4f6d5e8d3498316deb7065b3c529
wkt <waikon@waikontse.info>
2024-12-29 21:59:51 +0100

    Add CORS allowed list.

---
+++ src/main/kotlin/info/wkt/stock_exchange/configuration/WebMvcConfig.kt
@@ -0,0 +1,13 @@
+package info.wkt.stock_exchange.configuration
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebMvcConfig: WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        // Allow all origins
+        registry.addMapping("/**")
+    }
+}
